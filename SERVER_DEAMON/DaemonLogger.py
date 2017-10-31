326e64d7b331be522328720437bb6b825b0ff357
matshec <mmasakra@gmail.com>
2017-10-31 14:55:28 +0100

    Done basic daemon implementation with some internal testing

---
+++ SERVER_DEAMON/DaemonLogger.py
@@ -0,0 +1,25 @@
+from SERVER_DEAMON import Daemon
+import os,sys,time
+
+
+class DaemonLogger(Daemon.Daemon):
+
+    def run(self):
+        try:
+            file = open("/home/matshec/daemonfile.txt", "w+")
+            file.write("some textfrom deamon123")
+            file.close()
+            time.sleep(100)
+        except Exception:
+            sys.stderr.write("exception in 'run' occured")
+
+
+
+# pid = os.getpid()
+# fdd = open("pidfile","w+")
+# fdd.write(str(pid))
+# fdd.close()
+myDaemon = DaemonLogger("pidfile")
+myDaemon.start()
+
+myDaemon.stop()
