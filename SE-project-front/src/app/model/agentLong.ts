06ef6e6be5636a80432b560941dea0faa026ab27
patryk-zygmunt <patrykzygmuntzx@gmail.com.com>
2017-11-25 16:22:06 +0100

    init front wkoncu

---
+++ SE-project-front/src/app/model/agentLong.ts
@@ -0,0 +1,11 @@
+import {ServerInfo} from "./server_info";
+/**
+ * Created by Linus on 25.11.2017.
+ */
+export class AgentLong{
+  mac:number;
+  name:string;
+  agentId:number;
+  serverInfoResponse:ServerInfo;
+
+}
