199f0e126d9109c11d1c78b1bbeb0e86307642c7
matshec <mmasakra@gmail.com>
2017-11-25 16:09:43 +0100

    finished fformat_drive_data method

--- agent/collector.py
+++ agent/collector.py
@@ -13,7 +13,6 @@ class SystemDataCollector:
         except:
             return "error reading temp"
 
-
     #unints - mb
     def ram_usage(self):
         try:
@@ -23,25 +22,35 @@ class SystemDataCollector:
         except:
             return "read error occurred", "read error occurred"
 
-
-
     def __format_total_and_used_ram(self, raw_data:str):
         mem_data = raw_data.split("\\n")[1]
         mem_data = mem_data[4:]
         match_data = re.split(" +", mem_data)
         return match_data[1], match_data[2]
 
+
     def drive_space(self):
+        """:returns list of tuples with name,size,used_size [GB] eg [(a1,200,120),(b2,30,12)]"""
         try:
             raw_drive_data = self.__exec_sys_command("df", "-h")
             raw_drive_data = raw_drive_data.stdout
-            return raw_drive_data
+            drive_data = self.__format_drive_space_data(raw_drive_data)
+            return drive_data
+            #return raw_drive_data
         except:
-            return "error reading data",  "error reading data"
+            return "error reading data"
 
-    def __format_drive_space_data(self, raw_data:str):
-        pass
-       # match_iter = re.finditer(r"/sd\w+", str(raw_data))
+    def __format_drive_space_data(self, raw_data_str):
+        raw_data_list = raw_data_str.split(b"\n")
+        sd_list = list(filter(lambda x: b"/dev/sd" in x, raw_data_list))
+        ret_list = []
+        for dt in sd_list:
+            filtered_spaces = list(filter(lambda x: x != b'' ,re.split(b' ',dt)))
+            name = filtered_spaces[0][7:]
+            size = filtered_spaces[1]
+            used = filtered_spaces[2]
+            ret_list.append((str(name),str(size),str(used)))
+        return ret_list
 
     def processor_usage(self):
         try:
@@ -52,12 +61,11 @@ class SystemDataCollector:
             return "error reading"
 
     def __format_proc_usage(self,raw_data: str):
-            """":returns tuple user usage, system usage and unused power"""
+            """:returns tuple user usage, system usage and unused power"""
             cpu_data = raw_data.split("\\n")[2]
             cpu_data = cpu_data[9:]
             cpu_split = cpu_data.split(" ")
             return cpu_split[0].replace(",","."), cpu_split[3].replace(",","."), cpu_split[8].replace(",", ".")
-
 
     def disk_operations_in_progess(self):
             """"zwraca io operacji dyskowych w tej chwili (zwykle 0) nie wiem do końca czy oto chodziło"""
@@ -80,7 +88,5 @@ class SystemDataCollector:
             raw_data = sub.run([command, args],stdout=sub.PIPE)
             raw_data.check_returncode()
             return raw_data
-
-
 
 
